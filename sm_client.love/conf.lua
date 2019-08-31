a15ede70652354c82f88c2fb63df7447c5bf1ba0
idiscipulo <ikdiscipulo@gmail.com>
2019-08-30 19:20:17 -0700

    added terminal to .conf files

--- sm_client.love/conf.lua
+++ sm_client.love/conf.lua
@@ -1,4 +1,6 @@
-function love.conf()
+function love.conf(t)
     require("src/sm_game")
     require("src/sm_socket")
+
+    t.console = true
 end
--- sm_server.love/conf.lua
+++ sm_server.love/conf.lua
@@ -1,4 +1,6 @@
-function love.conf()
+function love.conf(t)
     require("src/sm_socket")
     require("src/sm_game")
+
+    t.console = true
 end
