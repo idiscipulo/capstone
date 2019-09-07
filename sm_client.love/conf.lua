d29019385e2bd3b069694564b6497d74b84319a6
idiscipulo <ikdiscipulo@gmail.com>
2019-09-06 18:14:31 -0700

    added timer package and set 60fps on client

--- sm_client.love/conf.lua
+++ sm_client.love/conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
     require("src/sm_game")
     require("src/sm_socket")
+    require("src/timer_util")
 
     t.console = true
 end
--- sm_client.love/main.lua
+++ sm_client.love/main.lua
@@ -1,9 +1,15 @@
 function love.load()
-    -- delete this later
+    timer_util = TimerUtil:new()
+    timer_util:set_fps(60)
 end
 
 function love.update()
+    timer_util:start_frame()
+
+    act_fps = timer_util:end_frame()
 end
 
 function love.draw()
+    print(act_fps)
+    love.graphics.print({{1, 1, 1, 1}, math.floor(act_fps)}, 0, 0)
 end
