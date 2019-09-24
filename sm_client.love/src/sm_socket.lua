fc80e9e280628f4dae625a14f8dbff8d37d0d010
Boice Wong (Student) <bwong8@sundevils.asu.edu>
2019-09-24 11:58:03 -0700

    Server talks with Client.
    Now, the server can manage clients through a table of client_ids (ip:port).
    The server will broadcast to the clients in that table the UDP packet.
    
    So far the UDP packet is: [player_pos_x, player_pos_y, mouse_x, mouse_y, client_id]
    Later, the UDP packet will be a game state

--- sm_client.love/src/sm_socket.lua
+++ sm_client.love/src/sm_socket.lua
@@ -1,57 +1,51 @@
---[[
 socket = require("socket") --import lua socket
+local address, port = "localhost", 8080
 udp = socket.udp()
-udp:setsockname('*', 8080) --for now just one instance, will allow a host to start this later
+udp:setpeername(address, port)
+udp:settimeout(0)
 
 SMSocket = {}
 SMSocket.__index = SMSocket
 
-function SMSocket:new()
-    local sm_socket = {}
+function SMSocket:new(player)
+	local sm_socket = {}
+	sm_socket.player = player
     setmetatable(sm_socket, SMSocket)
 
     return sm_socket
 end
 
 function SMSocket:update()
-	while true do --starts loop to listen
-		--receive data
-		local server_data = self.get_input()        
-        --send to server
-        self:send_state(client_data)	
+	--we cant't have loops in here because update gets called on a loop in sm_game
+	--send to server
+	self:send_state()
+	--receive data
+	local server_data = self:get_state()      
+	
 end
 
 
-function SMSocket:send_input()
-	udp:sendto(data, --server ip and port)
+function SMSocket:send_state()
+	udp:send(self.player:to_string())
 end
 
 function SMSocket:get_state()
 	--receive data
-	local data, ip, port = udp:receivefrom()
-	if(data == nil) then
-		break
-	end--if
-
-	local p_movement = split(data, '-') --[player_pos_x, player_pos_y, mouse_x, mouse_y]
-
-	--manage clients
-	--clients need to keep track of clients that are not themselves
-	local client_id = ip..":"..port
-	--if not this clients ip and port
-		--stick in table or not if it exists already
-		--use p_movement to update that client's position
-	--else --it is our client_id
+	local data = udp:receive()  --program is stalling here
+	if(data) then
+		print(data)
+		local p_movement = split(data, '-') --[player_pos_x, player_pos_y, mouse_x, mouse_y, client_id]
 
 	end--if
 	return data
 end
 
-function split(s, delimiter) --[player_pos_x, player_pos_y, mouse_x, mouse_y, client_name]
+function split(s, delimiter) --[player_pos_x, player_pos_y, mouse_x, mouse_y, client_id]
 	result = {}
 	for match in (s..delimiter):gmatch("(.-)"..delimiter) do
 		table.insert(result, match)
 	end --for
 	return result
 end --split
-]]
+
+return SMSocket
