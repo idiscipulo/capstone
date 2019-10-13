c4f4d4ba5b9259ff5ecd1a2beae6c955bbdbc865
Boice Wong (Student) <bwong8@sundevils.asu.edu>
2019-10-12 20:47:17 -0700

    Taiga #48 Character (NPC) Initial Inheritance
    Created the basic inheritance to entity and constructor for NPC
    
    npc.lua

---
+++ server.love/src/npc.lua
@@ -0,0 +1,10 @@
+NPC = {}
+NPC.__index = NPC
+setmetatable(NPC Entity) -- inherit from entity
+
+function NPC:new(id)
+    local npc = Entity:new()
+    setmetatable(npc, NPC)
+
+    return npc
+end
