baf63b1643655c5b6cba69764a18e7d104a53ebe
Boice Wong (Student) <bwong8@sundevils.asu.edu>
2019-10-12 20:55:58 -0700

    Taiga #51 Player Character (AI controlled) Initial Inheritance
    Initial inheritance to NPC and constructor made
    
    character_ai_controlled.lua

---
+++ server.love/src/character_ai_controlled.lua
@@ -0,0 +1,10 @@
+Character_AI = {}
+Character_AI.__index = Character_AI
+setmetatable(Character_AI, NPC) -- inherit from entity
+
+function Character_AI:new(id)
+    local character_ai = NPC:new()
+    setmetatable(character_ai, Character_AI)
+
+    return character_ai
+end
