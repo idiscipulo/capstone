d5111d08aef907eb3aeafb432f2ad39ae69357b0
jeragsda <jeragsda@asu.edu>
2020-04-30 22:19:41 -0700

    zainna

--- supamoba.love/src/ZainnaRaunor/ZainnaBloodDamage.lua
+++ supamoba.love/src/ZainnaRaunor/ZainnaBloodDamage.lua
@@ -9,7 +9,7 @@ function ZainnaBloodDamage:new(user)
     setmetatable(zainnaBloodDamage, ZainnaBloodDamage)
 
     -- set x, y, width, height, cooldown (in seconds), and name
-    zainnaBloodDamage:set(0, 0, 80, 80, 6, 'abilityExampleDirectDamage')
+    zainnaBloodDamage:set(0, 0, 80, 80, 6, 'zainnadamage')
 
     -- create description text
     zainnaBloodDamage.desc = font:printToCanvas('do incredible damage for a price', 189, 38, 'left')
@@ -31,6 +31,12 @@ function ZainnaBloodDamage:use(dir)
         if self.character.isAI then angle = dir 
         else angle = math.atan2(self.character.sprite.y - mouse.y, self.character.sprite.x - mouse.x) end
         self.character:takeDamage(self.damage / 4)
-        self.character:addBasicAttack(self.damage, angle, 0.4, 0)
+
+        --self.character:addBasicAttack(self.damage, angle, 0.4, 0, 'root', 2)
+        local ind = #self.character.basicAttacks + 1
+                                           --damage, char, ind, x, y, cooldown, name, speed, angle, delay, effect, effectTimer
+        local basicAttack = BasicAttack:new(self.damage, self.character, ind, self.character.sprite.x, self.character.sprite.y, 0.4, 'zainnadamage.sprite', self.character.basicSpeed, angle, 0)
+        self.character.basicAttacks[ind] = basicAttack
+        --self.character:addBasicAttack(self.damage, angle, 0.4, 0)
     end
 end
--- supamoba.love/src/ZainnaRaunor/ZainnaLifesteal.lua
+++ supamoba.love/src/ZainnaRaunor/ZainnaLifesteal.lua
@@ -9,7 +9,7 @@ function ZainnaLifesteal:new(user)
     setmetatable(zainnaLifesteal, ZainnaLifesteal)
 
     -- set x, y, width, height, cooldown (in seconds), and name
-    zainnaLifesteal:set(0, 0, 80, 80, 14, 'abilityExampleDirectDamage')
+    zainnaLifesteal:set(0, 0, 80, 80, 2, 'zainnaheal')
 
     -- create description text
     zainnaLifesteal.desc = font:printToCanvas('regain damage dealt as health', 189, 38, 'left')
@@ -30,6 +30,12 @@ function ZainnaLifesteal:use(dir)
         local angle = nil
         if self.character.isAI then angle = dir 
         else angle = math.atan2(self.character.sprite.y - mouse.y, self.character.sprite.x - mouse.x) end
-        self.character:addBasicAttack(self.damage, angle, 0.4, 0, 'lifesteal', 0)
+        --self.character:addBasicAttack(self.damage, angle, 0.4, 0, 'lifesteal', 0)
+        --self.character:addBasicAttack(self.damage, angle, 0.4, 0, 'root', 2)
+        local ind = #self.character.basicAttacks + 1
+                                           --damage, char, ind, x, y, cooldown, name, speed, angle, delay, effect, effectTimer
+        local basicAttack = BasicAttack:new(self.damage, self.character, ind, self.character.sprite.x, self.character.sprite.y, 0.4, 'zainnaheal.sprite', self.character.basicSpeed, angle, 0, 'lifesteal', 1)
+        self.character.basicAttacks[ind] = basicAttack
+        --self.character:addBasicAttack(self.damage, angle, 0.4, 0)
     end
 end
